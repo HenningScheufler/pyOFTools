e37ce2725ef3009ebde38809022ad2a66d9cf114
Henning Scheufler <henning.scheufler@web.de>
2025-12-30 12:27:28 +0100

    update version

--- src/pyOFTools/__init__.py
+++ src/pyOFTools/__init__.py
@@ -4,7 +4,7 @@ pyOFTools - Python tools for OpenFOAM
 A collection of Python utilities and tools for working with OpenFOAM.
 """
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 __author__ = "Henning Scheufler"
 __email__ = "henning.scheufler@dlr.de"
 
